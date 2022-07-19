75205ebf2c01cf62c0528d0a0d1fa269e77d3c50
Dave Morgan <dave@dmorg.org>
2022-07-19 01:53:47 +0100

    first save

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,13 @@
+<!-- src/lib/components/Header.svelte  -->
+
+<script>
+    let title = "Keziah Hobbs";
+    let subtitle = "Voice Actor"
+</script>
+
+<header>
+	<a href="/" sveltekit:prefetch >
+		<div>{title}</div>
+        <div>{subtitle}</div>
+	</a>
+</header>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,25 @@
+<script>
+   import '$lib/styles/app.scss'; 
+   import Header from '$lib/components/Header.svelte'
+</script>
+
+<svelte:head>
+	<title>keziah</title>
+</svelte:head>
+
+
+
+<div class="container">
+    <Header />
+    <slot />
+    footer
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		height: 100vh;
+	}
+</style>
